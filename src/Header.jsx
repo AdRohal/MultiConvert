2f38aaf524ff1358b0ab1c7849c4b3fd51e7fbd1
AdRohal <kradamrohal@hotmail.com>
2025-01-24 19:37:45 +0100

    Project V1.7.8: Fixing the Light Mode for Footer

--- src/Header.jsx
+++ src/Header.jsx
@@ -28,7 +28,7 @@ const Header = ({ darkMode, toggleDarkMode }) => {
         <span className="text-orange-500 text-xl ml-2">Multi-Convert</span>
       </div>
       <div className="flex items-center">
-        <nav className="flex items-center">
+        <nav className="flex items-center text-sm">
           <a href="/" className="font-bold">Home</a>
           <div className="h-6 border-l border-orange-500 mx-2"></div>
           <div className="relative" ref={dropdownRef}>
@@ -37,14 +37,14 @@ const Header = ({ darkMode, toggleDarkMode }) => {
             </button>
             <div className={`absolute bg-white shadow-md mt-4 border border-orange-500 ${dropdownOpen ? 'block' : 'hidden'}`} style={{ marginLeft: '-90px', width: '21rem' }}>
               <div className="flex flex-wrap">
-                <a href="/convert-document" className="block w-1/2 px-4 py-2 hover:bg-gray-100 border-r border-orange-500">Convert Document</a>
-                <a href="/convert-images" className="block w-1/2 px-4 py-2 hover:bg-gray-100">Convert Images</a>
+                <a href="/convert-document" className="block w-1/2 px-4 py-2 hover:bg-gray-100 border-r border-orange-500 text-sm">Convert Document</a>
+                <a href="/convert-images" className="block w-1/2 px-4 py-2 hover:bg-gray-100 text-sm">Convert Images</a>
               </div>
             </div>
           </div>
           <div className="h-6 border-l border-orange-500 mx-4"></div>
         </nav>
-        <button className="bg-orange-500 text-white px-4 py-2 ml-4 flex items-center justify-center rounded-full" style={{ marginLeft: '0px', paddingTop: '3px', paddingBottom: '3px', paddingRight: '9px', paddingLeft: '9px' }} onClick={toggleDarkMode}>
+        <button className="bg-orange-500 text-white px-4 py-2 ml-4 flex items-center justify-center rounded-full text-sm" style={{ marginLeft: '0px', paddingTop: '3px', paddingBottom: '3px', paddingRight: '9px', paddingLeft: '9px' }} onClick={toggleDarkMode}>
           {darkMode ? (
             <>
               <i className="fas fa-sun mr-2"></i> Light Mode
